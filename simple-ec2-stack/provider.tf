cb3aade48272b0166f1f1dd13862b0570411bdf0
suresh <sureshkumarr.s@gmail.com>
2019-11-26 16:22:03 +0530

    u

--- simple-ec2-stack/provider.tf
+++ simple-ec2-stack/provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
   region     = "${var.aws_region}"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "access_key" {}
+variable "secret_key" {}
 variable "aws_region" {}
 
